f4d866d055be1dd02bdd696076e4604dbfdf82a7
Michał Skwira <michalskwira@yahoo.com>
2018-10-24 22:13:11 +0200

    Change files

---
+++ script.js
@@ -0,0 +1,48 @@
+$(function(){
+  var carouselList = $("#carousel ul");
+  setInterval(changeSlide, 3000);
+  
+    function changeSlide(){
+      carouselList.animate({'marginLeft':-640}, 500, moveFirstSlide);
+
+      function moveFirstSlide(){
+        var  firstItem = carouselList.find("li:first"),
+             lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft:0});
+      }
+    }
+    function slideBack(){
+       carouselList.animate({'marginLeft':+640}, 500, moveFirstSlideBack);
+       function moveFirstSlideBack(){
+         var  firstItem = carouselList.find("li:first"),
+              lastItem = carouselList.find("li:last");
+         firstItem.before(lastItem);
+         carouselList.css({marginLeft:0});
+        }
+     }
+  $("#right").click(function(){
+    changeSlide();
+  });
+  $("#left").click(function(){
+    slideBack();
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
